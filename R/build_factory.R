b84d05026742e21fdbf3649349e5cb22491f6ec1
Jon Harmon <jonthegeek@gmail.com>
2020-06-02 17:35:53 -0500

    Added ability to make choices before the actual function return. (#27)
    The Travis errors continue to appear to be not-our-problem, and are pushing me toward getting Github Actions sorted out sooner rather than later.

--- R/build_factory.R
+++ R/build_factory.R
@@ -4,10 +4,12 @@
 #' @param ... Arguments for the factory function. Things on the RHS will be
 #'   evaluated before building your factory unless explicitly quoted with
 #'   \code{quote}. See examples.
-#' @param pass_dots A logical indicating whether the factory should accept
+#' @param .pass_dots A logical indicating whether the factory should accept
 #'   additional arguments (...) to pass on to methods. In order for this to
 #'   work, the manufactured function *must* also include dots, and the input
 #'   \code{fun} must indicate where those dots are used.
+#' @param .internal_variables A named list of additional code to run to create
+#'   additional variables used by the factory.
 #'
 #' @return A function factory.
 #' @export
@@ -24,18 +26,33 @@
 #' square(2)
 #' y <- 7
 #' square(2)
+#'
+#' base_bins <- build_factory(
+#'   .internal_variables = list(
+#'     nclass_fun = switch(
+#'       type,
+#'       Sturges = nclass.Sturges,
+#'       scott = nclass.scott,
+#'       FD = nclass.FD,
+#'       stop("Unknown type", call. = FALSE)
+#'     )
+#'   ),
+#'   fun = function(x) {
+#'     (max(x) - min(x) / nclass_fun(x))
+#'   },
+#'   type
+#' )
+#' base_bins("Sturges")
 build_factory <- function(fun,
                           ...,
-                          pass_dots = FALSE) {
+                          .pass_dots = FALSE,
+                          .internal_variables = NULL) {
   if (!...length()) {
     stop("You must provide at least one argument to your factory.")
   }
 
-  # To deal with *all* possibilities, we need to enquos the dots (so nobody
-  # tries to evaluate them).
   dots <- rlang::enquos(...)
   dots_names <- names(rlang::quos_auto_name(dots))
-
   args <- as.list(dots) %>%
     purrr::modify_if(
       ~ (rlang::is_quosure(.) && rlang::quo_is_null(.)),
@@ -56,6 +73,24 @@ build_factory <- function(fun,
     purrr::flatten() %>%
     purrr::set_names(dots_names)
 
+  # I can't find a way to do this neatly with rlang. I want the user to pass
+  # bare code in a list, and I don't want to evaluate that code. !!! unquotes
+  # and thus breaks, so instead I need to enexpr and then pull the result apart.
+  to_do <- rlang::enexpr(.internal_variables)
+  if (length(to_do)) {
+    if (as.character(to_do[[1]]) != "list") {
+      stop(".internal_variables must be a named list of code.")
+    }
+    to_do[[1]] <- NULL
+
+    # We need to catch anything in .internal_variables when we update the
+    # function.
+    dots_names <- rlang::list2(!!!names(to_do), !!!dots_names)
+  }
+
+  # multiple_funs <- rlang::enexpr(fun)
+  # return(multiple_funs)
+
   # We also need to update the function body.
   body(fun) <- purrr::reduce(
     dots_names,
@@ -75,19 +110,31 @@ build_factory <- function(fun,
     )
   })
 
-  if (pass_dots) {
+  if (.pass_dots) {
     args <- rlang::pairlist2(
       !!!args,
       "..." =
       )
     old_fun <- fun
-    body(fun) <- body_insert(
-      fn_body = body(fun),
-      insertion = quote(!!!dots),
-      before = quote(...)
-    )
+
+    # If they want to pass ... to the child, we *add* !!!dots. Else we replace
+    # ... with !!!dots.
+    if ("..." %in% names(formals(fun))) {
+      body(fun) <- body_insert(
+        fn_body = body(fun),
+        insertion = quote(!!!dots),
+        before = quote(...)
+      )
+    } else {
+      body(fun) <- body_replace(
+        fn_body = body(fun),
+        target = quote(...),
+        replacement = quote(!!!dots)
+      )
+    }
+
     if (identical(old_fun, fun)) {
-      stop("fun must contain ... when pass_dots is TRUE.")
+      stop("fun must contain ... when .pass_dots is TRUE.")
     }
 
     # Update child_fun.
@@ -103,6 +150,21 @@ build_factory <- function(fun,
       )
   }
 
+  if (length(to_do)) {
+    child_fn <- purrr::reduce2(
+      names(to_do),
+      to_do,
+      ~ body_insert(
+        fn_body = ..1,
+        insertion = rlang::call2(
+          `<-`,
+          rlang::sym(..2),
+          ..3
+        )
+      ),
+      .init = child_fn
+    )
+  }
 
   return(
     rlang::new_function(
@@ -112,3 +174,46 @@ build_factory <- function(fun,
     )
   )
 }
+
+# build_factory.list <- function(fun,
+#                                ...,
+#                                .pass_dots = FALSE,
+#                                .internal_variables = NULL) {
+#   # The structure of the list is somewhat complicated and strict. Check that
+#   # first.
+#   if (length(list) != 1) {
+#     stop("We can currently only make the function choice based on",
+#          " 1 argument.",
+#          " If you need something more complex, perhaps try",
+#          " .internal_variables.")
+#   }
+#   control_variable <- names(fun)
+#   if (! (control_variable %in% dots_names)) {
+#     stop(
+#       "For now at least, you must supply the name of the control variable",
+#       "in the ... of the build_factory call."
+#     )
+#   }
+#
+#   new_call <- rlang::call2(
+#     switch,
+#     rlang::sym(control_variable),
+#     !!!fun[[1]],
+#     quote(stop("Unknown value for function chooser."))
+#   )
+#
+#   new_fun <- rlang::new_function(
+#     args = args,
+#     body = new_call,
+#     env = rlang::caller_env()
+#   )
+#
+#   return(
+#     build_factory(
+#       new_fun,
+#       ...,
+#       .pass_dots,
+#       .internal_variables
+#     )
+#   )
+# }
--- tests/testthat/test-build_factory.R
+++ tests/testthat/test-build_factory.R
@@ -85,36 +85,28 @@ test_that("dots [...] as arguments work.", {
 })
 
 test_that("Factories can pass dots.", {
-  expect_error(
-    {
-      number_format <- build_factory(
-        fun = function(x, ...) {
-          scales::number(
-            x,
-            accuracy = accuracy, scale = scale, prefix = prefix,
-            suffix = suffix, big.mark = big.mark, decimal.mark = decimal.mark,
-            trim = trim, ...
-          )
-        },
-        accuracy = NULL,
-        scale = 1,
-        prefix = "",
-        suffix = "",
-        big.mark = " ",
-        decimal.mark = ".",
-        trim = TRUE,
-        pass_dots = TRUE
+  number_format <- build_factory(
+    fun = function(x, ...) {
+      scales::number(
+        x,
+        accuracy = accuracy, scale = scale, prefix = prefix,
+        suffix = suffix, big.mark = big.mark, decimal.mark = decimal.mark,
+        trim = trim, ...
       )
     },
-    NA
+    accuracy = NULL,
+    scale = 1,
+    prefix = "",
+    suffix = "",
+    big.mark = " ",
+    decimal.mark = ".",
+    trim = TRUE,
+    .pass_dots = TRUE
   )
 
-  expect_error(
-    expect_identical(
-      scales::number_format(width = 8)(1:10 * 10000),
-      number_format(width = 8)(1:10 * 10000)
-    ),
-    NA
+  expect_identical(
+    scales::number_format(width = 8)(1:10 * 10000),
+    number_format(width = 8)(1:10 * 10000)
   )
 
   expect_error(
@@ -123,8 +115,88 @@ test_that("Factories can pass dots.", {
         mean(x * multiple)
       },
       multiple,
-      pass_dots = TRUE
+      .pass_dots = TRUE
     ),
     regexp = "fun must contain"
   )
+
+  number_format2 <- factory::build_factory(
+    fun = function(x) {
+      scales::number(
+        x,
+        accuracy = accuracy, scale = scale, prefix = prefix, suffix = suffix,
+        big.mark = big.mark, decimal.mark = decimal.mark, trim = trim, ...
+      )
+    },
+    accuracy = NULL,
+    scale = 1,
+    prefix = "",
+    suffix = "",
+    big.mark = " ",
+    decimal.mark = ".",
+    trim = TRUE,
+    .pass_dots = TRUE
+  )
+
+  expect_identical(
+    scales::number_format(width = 8)(1:10 * 10000),
+    number_format2(width = 8)(1:10 * 10000)
+  )
+  expect_identical(
+    formals(number_format2(width = 8)),
+    as.pairlist(alist(x = ))
+  )
+})
+
+test_that("Can build factories that branch into functions by variable.", {
+  base_bins <- build_factory(
+    .internal_variables = list(
+      nclass_fun = switch(
+        type,
+        Sturges = grDevices::nclass.Sturges,
+        scott = grDevices::nclass.scott,
+        FD = grDevices::nclass.FD,
+        stop("Unknown type", call. = FALSE)
+      )
+    ),
+    fun = function(x) {
+      (max(x) - min(x)) / nclass_fun(x)
+    },
+    type
+  )
+  x <- rnorm(3 * 100, sd = c(1, 5, 15))
+  expect_identical(
+    base_bins("Sturges")(x),
+    (max(x) - min(x)) / nclass.Sturges(x)
+  )
+
+  expect_error(
+    build_factory(
+      .internal_variables = c(nclass_fun = "this"),
+      fun = function(x) {
+        (max(x) - min(x)) / nclass_fun(x)
+      },
+      type
+    ),
+    regexp = "internal_variables must be a named list"
+  )
+
+  # I have not finished this test but don't want to delete it.
+  # plot_dev <- build_factory(
+  #   fun = list(
+  #     ext = list(
+  #       ps = function(path, ...) {
+  #         grDevices::postscript(
+  #           file = filename, ..., onefile = FALSE, horizontal = FALSE,
+  #           paper = "special"
+  #         )
+  #       },
+  #       png = function(...) {
+  #         grDevices::png(..., res = dpi, units = "in")
+  #       }
+  #     )
+  #   ),
+  #   ext,
+  #   dpi = 96
+  # )
 })
